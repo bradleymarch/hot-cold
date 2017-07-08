20fe97e8a37dbad218ae0d94fdd93b50393b0fbb
bradleymarch <bradleymarch@gmail.com>
2017-07-08 16:01:16 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import './reset.css'
 import Game from './components/Game';
 import registerServiceWorker from './registerServiceWorker';
-
+//comment
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
